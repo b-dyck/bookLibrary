f113758460a91c06e7d51e15bfcc3092dbc802f0
Bryce Dyck <bryce.dyck@alumni.ubc.ca>
2023-03-11 09:31:26 -0800

    Install eslint and prettier to the project

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;    
 }
 
-//fucntion to add book to library based on user input
+//fucntion to add book to library dataset based on user input
 function addBookToLibrary(e) {
 
     let title=$titleInput.value
@@ -57,31 +57,42 @@ function addBookToLibrary(e) {
     e.preventDefault()
 }
 
+//Loops through library dataset and creates DOM Element for each book in library div
 function displayLibrary() {
     for (let i in myLibrary) {
         createBookCard(myLibrary[i])
+        document.querySelectorAll(`.${myLibrary[i].title}`).setAttribute("data-index", i)
     }
 }
 
+// Creates display for book by DOM manipulation. Appends book card to the Library div
 function createBookCard(book) {
     
     title.textContent = book.title
     author.textContent = book.author
     pages.textContent = `${book.pages} pages`
     removeBtn.textContent = 'Remove'
+    removeBtn.addEventListener('click', removeBook() // NOT FINISHED THIS RETURN TO THIS )
 
     if (book.isRead) {
         readBtn.textContent = 'Read'
     } else {
         readBtn.textContent='Not Read'
     }
+    //Sets id on remove button to allow for removal of DOM element and inout in library dataset
+    removeBtn.classList.add = book.title
+    card.classList.add = book.title
 
-    card.appendChild(title)
     card.appendChild(author)
     card.appendChild(pages)
     btnGroup.appendChild(readBtn)
     btnGroup.appendChild(removeBtn)
     card.appendChild(btnGroup)
     $library.appendChild(card)
+}
+
+function removeBook(index) {
+    $library.removeChild(document.querySelector(`div[data-index=${index}]`))
+    myLibrary.splice(index, 1)
 }
 
